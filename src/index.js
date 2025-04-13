e4483e69ca793824319a96bbdafbe386ef3ef649
erfan-sahil <erfansahil20@gmail.com>
2025-04-13 23:18:21 +0600

    check http req and res

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ const port = process.env.PORT || 4500;
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
+
+app.get("/test", (req, res) => {
+  console.log("App is running successfully");
+  res.send({ msg: "App is runnnig successfully" });
+});
