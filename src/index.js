69ebeb529738d7c3059c2ea69e71e951378b423f
erfan-sahil <erfansahil20@gmail.com>
2025-04-13 23:33:48 +0600

    use nodemon and morgan

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-
+const morgan = require("morgan");
 const port = process.env.PORT || 4500;
+
+app.use(morgan("dev"));
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
