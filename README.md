709640f54c8f6b341c5c7a97cadcdcd0a1252b9a
erfan-sahil <erfansahil20@gmail.com>
2025-04-14 12:51:55 +0600

    use some builtin and error handling middleware

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Packages that are installed in this application
 
    1. Morgan
    2. Nodemon
+
+---
+
+Folder Structure
+
+src
+
+1. Index.js
---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     mongoose.connect(() => {
+//       console.log("App is connect with the database");
+//     });
+//   } catch (error) {
+//     console.log("Something is wrong. Database not connected", error);
+//   }
+// };
+
+// module.exports = connectDB;
