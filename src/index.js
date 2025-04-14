c0454a8bae0a851820e4bbb2046ccd4edaed481d
erfan-sahil <erfansahil20@gmail.com>
2025-04-14 13:58:04 +0600

    env and gitignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const app = require("./app");
-
-const port = process.env.PORT || 4500;
+const { port } = require("./secret");
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
---
+++ src/secret.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+const port = process.env.PORT || 3500;
+
+module.exports = {
+  port,
+};
