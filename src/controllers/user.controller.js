2e7628faadf4d62daee6a6ec547d68f98d09a7e3
erfan-sahil <erfansahil20@gmail.com>
2025-04-15 07:26:57 +0600

    fetched user with pagination and search user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,51 @@
 const createError = require("http-errors");
+const { userModel } = require("../models/user.model");
 
 const getUsers = async (req, res, next) => {
   try {
+    const search = req.query.search || "";
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 7;
+
+    const searchRegExp = new RegExp(".*" + search + ".*", "i");
+
+    const searchFilter = {
+      isAdmin: { $ne: true },
+      $or: [
+        { name: { $regex: searchRegExp } },
+        { email: { $regex: searchRegExp } },
+        { phone: { $regex: searchRegExp } },
+      ],
+    };
+
+    const options = {
+      password: 0,
+    };
+    const users = await userModel
+      .find(searchFilter, options)
+      .limit(limit)
+      .skip((page - 1) * limit);
+
+    const count = await userModel.find(searchFilter).countDocuments();
+
+    if (!users) {
+      throw createError(404, "No users found");
+    }
+    if (!users) {
+      console.log("Could not find any user");
+      res.status(400).json({
+        message: "Could not find any user",
+      });
+    }
     res.status(200).json({
-      msg: "Fetched all the users",
+      message: "Fetched all the users",
+      payload: users,
+      pagination: {
+        totalPages: Math.ceil(count / limit),
+        currentPage: page,
+        previousPage: page - 1 > 0 ? page - 1 : null,
+        nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+      },
     });
   } catch (error) {
     next(error);
